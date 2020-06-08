d849d700453aa3e0a4b313c8da2e5ea0f20e5035
Martín Costabal <micostabal@uc.cl>
2020-06-08 12:29:02 -0400

    Folder change

---
+++ Instances/MVRPD.py
@@ -0,0 +1,27 @@
+from gurobipy import *
+from Instance import Instance
+import os
+
+class MVRPD:
+ 
+    def __init__(self, instName = ''):
+        pass
+
+    def createInstance(self): pass
+
+    def exportMPS(self, writePath = os.path.join(os.path.pardir, 'MIPLIB')): pass
+
+    def genNeighborhoods(self): pass
+
+    def genLazy(self): pass
+
+    def genTestFunction(self): pass
+
+    def run(self): pass
+
+    def analyzeRes(self): pass
+
+    def visualizeRes(self): pass
+
+
+if __name__ == '__main__': pass
