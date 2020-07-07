a59ecff7b77afc27c755f90b8e8a08e4a3f39f22
Martín Costabal <micostabal@uc.cl>
2020-07-07 14:14:07 -0400

    file results and experiments

---
+++ Instances/Experiments.py
@@ -0,0 +1,17 @@
+import os
+from IRP import runSeveralIRP
+from IRPCS import runSeveralIRPCS
+from MVRPD import runSeveralMVRPD
+from VRP import runSeveralVRP
+from OISRC import runSeveralOISRC
+
+
+
+
+IRPinst = []
+IRPCSinst = []
+MVRPDinst = []
+OISRCinst = []
+
+
+if __name__ == '__main__': pass
